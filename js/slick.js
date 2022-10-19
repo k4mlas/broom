14f788329edb2a26197e331264bc732f9f1a4df7
Kamil Łaskarzewski <k4mlas@gmail.com>
2022-10-19 20:58:43 +0200

    working opinions

---
+++ js/slick.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+	$('.').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 2500,
+		arrows: false,
+        
+        
+		mobileFirst: true,
+		responsive: [
+			{
+				breakpoint: 700,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1,
+                    
+				},
+			},
+		],
+	});
+});
